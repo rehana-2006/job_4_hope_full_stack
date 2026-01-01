d0ea4e8207d5597780e5fafba15512feb762f3f9
rehana-2006 <rehanaprijil@gmail.com>
2026-01-01 18:39:25 +0530

    fixed issue

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from jose import JWTError, jwt
+import jwt
+from jwt import PyJWTError
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -48,7 +49,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-    except JWTError:
+    except PyJWTError:
         raise credentials_exception
     user = db.query(models.User).filter(models.User.email == email).first()
     if user is None:
